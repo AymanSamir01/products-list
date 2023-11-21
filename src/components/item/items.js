0fd342aa2cc92945aafe8009657bd731e64d1ad8
AymanSamir01 <oa1827166@gmail.com>
2023-11-21 23:47:11 +0200

    increase and decrease quantity

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const { items, del, update } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
 
-export default Items
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+          <p>
+            <button
+              className="increase"
+              onClick={() => {
+                update(item.id, 1, item.quantity);
+              }}
+            >
+              +
+            </button>
+            {item.quantity}
+            <button
+              className="decrease"
+              onClick={() => {
+                update(item.id, -1);
+              }}
+            >
+              -
+            </button>
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Edit</p>
+        <p>Quantity</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
